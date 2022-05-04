c5eff9237a52a97329644b7897ce1a3bb4de2a57
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-05-04 21:47:44 +0300

    little comment

--- mysite/djrfapis/seralizers.py
+++ mysite/djrfapis/seralizers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Student
 
 
+# studentSerializer for serializing student data
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
