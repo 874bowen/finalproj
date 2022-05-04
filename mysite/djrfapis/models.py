932a746c04f3b7f4a3d6625a176140486e924323
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-05-04 21:48:25 +0300

    little comment for the model

--- mysite/djrfapis/models.py
+++ mysite/djrfapis/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 # Create your models here.
-
+# students table here
 class Student(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
