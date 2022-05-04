dc3ecf650756d9bace31b81d1bd1e4eef1d44166
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-05-04 19:22:24 +0300

    post method for student

--- mysite/djrfapis/views.py
+++ mysite/djrfapis/views.py
@@ -3,11 +3,22 @@ from rest_framework.views import APIView
 
 from rest_framework.response import Response
 
+from .seralizers import StudentSerializer
+from .models import Student
+from rest_framework.permissions import IsAuthenticated
+
 
 class TestView(APIView):
+    permission_classes = (IsAuthenticated,)
+
     def get(self, request, *args, **kwargs):
-        data = {
-            'username': 'admin',
-            'no_of_years_active': 10
-        }
-        return Response(data)
+        qs = Student.objects.filter(id =1).first()
+        seralizer = StudentSerializer(qs)
+        return Response(seralizer.data)
+
+    def post(self, request, *args, **kwargs):
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
