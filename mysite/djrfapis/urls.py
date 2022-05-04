1139a0c191aac550774f7ee6399646c4b08c7939
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-05-04 19:20:51 +0300

    token authentication for login

--- mysite/djrfapis/urls.py
+++ mysite/djrfapis/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, reverse_lazy, include
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 app_name='apis'
 urlpatterns = [
     path('', views.TestView.as_view(), name='apis'),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('token/', obtain_auth_token, name="obtain")
 ]
